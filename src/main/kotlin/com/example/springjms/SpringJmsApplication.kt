df8f10895ffa4f0242855c47815be85275f8a247
Vinicius R <vrodrigues.code@gmail.com>
2022-08-31 09:09:16 -0300

    First commit, spring initializr

---
+++ src/main/kotlin/com/example/springjms/SpringJmsApplication.kt
@@ -0,0 +1,12 @@
+package com.example.springjms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJmsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringJmsApplication>(*args)
+}
+
---
+++ src/test/kotlin/com/example/springjms/SpringJmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springjms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringJmsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
